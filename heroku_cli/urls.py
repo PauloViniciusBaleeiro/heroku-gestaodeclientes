853adba06c2bd60bfc951eb756d94635324498ce
Paulo Vinicius Baleeiro Ferreira <paulo.baleeiro@birmind.com.br>
2018-12-19 22:52:45 -0200

    changes

--- heroku_cli/urls.py
+++ heroku_cli/urls.py
@@ -15,11 +15,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from clientes import urls as cli_urls
 from django.contrib.auth import views as auth_views
+from clientes import urls as cli_urls
+from home import urls as home_urls
 
 urlpatterns = [
-    path('', include(cli_urls)),
+    path('', include(home_urls)),
+    path('clientes/', include(cli_urls)),
     path('login/', auth_views.LoginView.as_view(), name='login'),
     path('logout', auth_views.LogoutView.as_view(), name='logout'),
     path('admin/', admin.site.urls),
