0c02529b9c72d2252d274d2e0f40812775e90c4f
paulo <pauloviniciusbf@gmail.com>
2019-01-06 21:20:21 -0200

    configuring to heroku

--- heroku_cli/settings.py
+++ heroku_cli/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 
 import os
 from decouple import config
+from dj_database_url import parse as dburl
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -26,7 +27,7 @@ SECRET_KEY = config('SECRET_TEXT')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', cast=bool, default=True)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['heroku-cli-manager.herokuapp.com', 'localhost']
 
 
 # Application definition
